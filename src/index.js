a7bc88997fc620371e7176794e128b275f77498c
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-02-24 13:30:04 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+// const myTalkers = require('./talker.json');
+
+const talkersPath = path.resolve(__dirname, './talker.json');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +16,32 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+const readTalkers = async () => {
+  try {
+    const talkersJson = await fs.readFile(talkersPath);
+    const talkers = JSON.parse(talkersJson);
+    console.log(talkers);
+    return talkers;
+  } catch (error) {
+    console.error('Erro');
+  }
+};
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readTalkers();
+  const errorCase = [];
+  if (!talkers) {
+    return errorCase;
+  }
+  return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+function main() {
+  readTalkers();
+}
+
+main();
